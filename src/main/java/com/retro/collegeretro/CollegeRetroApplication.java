99a783a68bdebe914b7ee3f61a284d43cac8952c
Gelberdaniel19 <gelberdaniel19@gmail.com>
2021-03-04 19:36:45 -0500

    Initial Commit

---
+++ src/main/java/com/retro/collegeretro/CollegeRetroApplication.java
@@ -0,0 +1,13 @@
+package com.retro.collegeretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeRetroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeRetroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/retro/collegeretro/CollegeRetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retro.collegeretro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeRetroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

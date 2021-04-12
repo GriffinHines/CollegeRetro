fb8f09c3ca3aeead0608a328ad45255ffd6efae9
Michael Rehman <michael@michaelrehman.com>
2021-04-12 19:09:39 -0400

    Fix scraping

--- src/main/java/com/retro/collegeretro/Controller/HomepageController.java
+++ src/main/java/com/retro/collegeretro/Controller/HomepageController.java
@@ -3,13 +3,13 @@ package com.retro.collegeretro.Controller;
 import com.retro.collegeretro.Model.Listing;
 import com.retro.collegeretro.Model.User;
 import com.retro.collegeretro.Repository.ListingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.List;
 
 @Controller
 public class HomepageController {
@@ -22,19 +22,9 @@ public class HomepageController {
 
     @GetMapping("/")
     public String getHomepage(@SessionAttribute(required = false) User user, Model model) {
-        // TODO(michaelrehman): filter to just a few ones
-        List<Listing> listings = listingRepository.findAll();
+        Page<Listing> all = listingRepository.findAll(PageRequest.of(0, 5));
+        model.addAttribute("listings", all);
 
-        // Sample (not saved to database)
-        Listing listing = new Listing();
-        listing.setListingName("name");
-        listing.setCategory("cate");
-        listing.setDescription("some desc");
-        listing.setOpen(true);
-        listing.setPriceInCents(123);
-        listing.setQuantity(12);
-        listings.add(listing);
-        model.addAttribute("listings", listings);
         return "index";
     }
 
--- src/main/java/com/retro/collegeretro/Repository/ListingRepository.java
+++ src/main/java/com/retro/collegeretro/Repository/ListingRepository.java
@@ -1,7 +1,10 @@
 package com.retro.collegeretro.Repository;
 
 import com.retro.collegeretro.Model.Listing;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+    Page<Listing> findAll(Pageable pageable);
 }

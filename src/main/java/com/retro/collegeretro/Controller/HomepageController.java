600ffb6e8c308cfb0592a485f25b701a1f4adabd
georgef7 <138088coding@UGA*>
2021-04-21 02:21:34 -0400

    Added Privacy policy and checkbox at signup page

--- src/main/java/com/retro/collegeretro/Controller/HomepageController.java
+++ src/main/java/com/retro/collegeretro/Controller/HomepageController.java
@@ -40,4 +40,8 @@ public class HomepageController {
         return new RedirectView("/");
     }
 
+    @GetMapping("/privacy")
+        public String getPrivacyPolicy() {
+            return "privacy";
+    }
 }
